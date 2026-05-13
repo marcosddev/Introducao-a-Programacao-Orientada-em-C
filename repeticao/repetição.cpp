1ee58d0fdbd602dade5663fccb03a4bf9426c8d8
Marcos Davi <129192674+00Eden@users.noreply.github.com>
2026-05-13 17:20:12 -0300

    Rename repetição.cpp to repeticao/repetição.cpp

--- repeticao/repetição.cpp
+++ repeticao/repetição.cpp
@@ -5,15 +5,15 @@ int main(){
     qpar=0;
     qimpar=0;
     for(int i=1; i<=15; i++){
-        printf("\n Digite o %dº número: ", i);
+        printf("\n Digite o %dÂº nÃºmero: ", i);
         scanf("%d", &num);
         if(num%2==0)
             qpar++;
         else
         qimpar++;
     }
-    printf("\n A quantidade de números pares é %d", qpar);
-    printf("\n A quantidade de números impares é %d", qimpar);
+    printf("\n A quantidade de nÃºmeros pares Ã© %d", qpar);
+    printf("\n A quantidade de nÃºmeros impares Ã© %d", qimpar);
     return(0);
             
         }
