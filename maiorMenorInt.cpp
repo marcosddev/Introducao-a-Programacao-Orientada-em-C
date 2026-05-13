882f07adfb257861c3c517ec6fc27d93aca94895
Marcos Davi <129192674+00Eden@users.noreply.github.com>
2026-05-13 14:06:40 -0300

    Add program to compare two floating-point numbers

---
+++ maiorMenorInt.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+float num1, num2;
+printf("Digite o primeiro número: ");
+scanf("%f", &num1);
+printf("Digite o segundo número: ");
+scanf("%f", &num2);
+if (num1 > num2) {
+ printf("%.2f maior que %.2f", num1, num2);
+ } else if (num2 > num1) {
+ printf("%.2f maior que %.2f", num2, num1);
+ } else {
+ printf("Os dois sao iguais\n");
+ }
+ return 0;
+}
