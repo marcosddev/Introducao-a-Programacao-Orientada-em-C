342d142edfbf9014e823eb985d6d3f66c45615e0
Marcos Davi <129192674+00Eden@users.noreply.github.com>
2026-05-13 14:14:14 -0300

    Update and rename somatorio.cpp to somatorioProduto.cpp

--- somatorioProduto.cpp
+++ somatorioProduto.cpp
@@ -1,19 +1,29 @@
-#include<stdio.h>
+#include<stdio.h
+>
 int main() {
 int qtd, i, num;
 int somatorio = 0;
+long produtorio= 1;
 printf("Digite a quantidade de números: ");
 scanf("%d", &qtd);
 printf("===============================\n");
-printf(" Leitura de %d números\n", qtd);
+printf(" Leitura de %d números
+\n", qtd);
 printf("===============================\n");
-for (i = 1; i <= qtd; i++) {
+i=1;
+while(i<=qtd){
 printf("Digite o %d º número", i );
 scanf("%d", &num);
-somatorio += num;
+somatorio += num
+;
+produtorio *= num
+;
+i++;
 }
 printf("===============================\n");
 printf("Somatório: %d\n", somatorio);
+printf(“Produtório: %ld\
+n", produtorio);
 printf("===============================\n");
 return 0;
 }
