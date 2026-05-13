939c2d58ad8126e10d18454104c8c5c6a1be98da
Marcos Davi <129192674+00Eden@users.noreply.github.com>
2026-05-13 13:27:17 -0300

    Rename kkçp9.cpp to ComparadorNumeros.cpp

--- ComparadorNumeros.cpp
+++ ComparadorNumeros.cpp
@@ -3,9 +3,9 @@
 int main(){
 	float imcnum2;
 	
-	printf("Digite o primeiro número: ");
+	printf("Digite o primeiro nÃºmero: ");
 	scanf("%f", &num1);
-	printf("Digite o segundo número: ");
+	printf("Digite o segundo nÃºmero: ");
 	scanf("%f", &num2);
 	if (num1 > num2) {
         printf("%.2f maior que %.2f", num1, num2);
